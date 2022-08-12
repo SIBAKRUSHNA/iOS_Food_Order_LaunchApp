29fe8ab1636a3d7eb38129c833110b8326233696
Siba Krushna Biswal <siba.biswal@mobiotics.com>
2022-08-12 20:23:08 +0530

    app init

---
+++ Food Order/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Order
+//
+//  Created by Mobiotics on 12/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
